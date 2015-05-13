9923d6aaf62ca8780a5f1b0c06cceb82e10f4311
Predrag Gruevski <predrag.gruevski@gmail.com>
2015-05-12 22:37:21 -0400

    Added generated graph statistics.

--- src/graphgen2/graphgen2.cpp
+++ src/graphgen2/graphgen2.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <exception>
+#include <algorithm>
 #include "./common.h"
 #include "./io.h"
 
@@ -18,7 +19,31 @@ using namespace std;
 static void printGraphStats(const vertex_t * const nodes,
                             const vector<vid_t> * const edges,
                             const vid_t cntNodes) {
-  // none yet
+  vid_t totalEdges = 0;
+  vid_t maxEdges = 0;
+  vid_t minNonZeroEdges = -1;
+  vid_t numDisconnectedNodes = 0;
+
+  for (vid_t i = 0; i < cntNodes; ++i) {
+    vid_t size = edges[i].size();
+    totalEdges += size;
+    maxEdges = max(maxEdges, size);
+
+    if (size == 0) {
+      ++numDisconnectedNodes;
+    } else {
+      minNonZeroEdges = min(minNonZeroEdges, size);
+    }
+  }
+
+  cout << "\nGraph stats:\n";
+  cout << "  total nodes: " << cntNodes << '\n';
+  cout << "  total edges: " << totalEdges << '\n';
+  cout << "  avg edges per node: " << static_cast<double>(totalEdges) / cntNodes << '\n';
+  cout << "  node max edges: " << maxEdges << '\n';
+  cout << "  connected node min edges: " << minNonZeroEdges << '\n';
+  cout << "  # of disconnected nodes: " << numDisconnectedNodes << '\n';
+  cout << endl;
 }
 
 int main(int argc, char *argv[]) {
