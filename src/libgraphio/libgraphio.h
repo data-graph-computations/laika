05ce75213c7d9203cfdbbbe4af43dce4bd007606
William C. Hasenplaugh <whasenpl@mit.edu>
2015-11-19 13:01:56 -0500

    added huge graph support switch

--- src/libgraphio/libgraphio.h
+++ src/libgraphio/libgraphio.h
@@ -4,7 +4,15 @@
 #include <string>
 #include <cinttypes>
 
+#ifndef HUGE_GRAPH_SUPPORT
+  #define HUGE_GRAPH_SUPPORT 1
+#endif
+
+#if HUGE_GRAPH_SUPPORT
+typedef int64_t vid_t;
+#else
 typedef int32_t vid_t;  // vertex id type
+#endif
 
 class EdgeListBuilder {
  public:
