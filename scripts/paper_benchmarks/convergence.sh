88248b7901a46bc434eb89cc673ac642f0055617
Predrag Gruevski <predrag@kensho.com>
2018-05-05 01:04:20 -0400

    Fix bugs in convergence script. Add other schedulers.

--- scripts/paper_benchmarks/convergence.sh
+++ scripts/paper_benchmarks/convergence.sh
@@ -15,21 +15,49 @@ default_compute_params="$default_compute_params RUN_CONVERGENCE_EXPERIMENT=1"
 
 input_name='rand.2'
 
+hilbert_inputs="./input_data/hilbert-graphs/${input_name}.binadjlist"
+hilbert_inputs="$hilbert_inputs ./input_data/hilbert-graphs/${input_name}.node.simple"
+
+unordered_inputs="./input_data/random-graphs/${input_name}.binadjlist"
+unordered_inputs="$unordered_inputs ./input_data/random-graphs/${input_name}.node.simple"
+
+
 make clean
 make PARALLEL=1 build-libgraphio
 make D1_NUMA=1 NUMA_WORKERS=48 CHUNK_BITS=11 \
      $default_compute_params build-graph-compute
 taskset -c 0-47 ./src/graph_compute/compute "$convergence_factor" \
-    ./input_data/hilbert-graphs/${input_name}.binadjlist \
-    ./input_data/hilbert-graphs/${input_name}.node.simple \
-    >${input_name}-hilbert-parallel-convergence-laika.txt
+    $hilbert_inputs
+    >./results/${input_name}-hilbert-parallel-convergence-laika.txt
 
 
 make clean
 make PARALLEL=1 build-libgraphio
 make D0_BSP=1 $default_compute_params build-graph-compute
 taskset -c 0-47 ./src/graph_compute/compute "$convergence_factor" \
-    ./input_data/hilbert-graphs/${input_name}.binadjlist \
-    ./input_data/hilbert-graphs/${input_name}.node.simple \
-    >${input_name}-hilbert-parallel-convergence-laika.txt
+    $hilbert_inputs
+    >./results/${input_name}-hilbert-parallel-convergence-bsp.txt
 
+
+make clean
+make PARALLEL=1 build-libgraphio
+make D1_CHROM=1 $default_compute_params build-graph-compute
+taskset -c 0-47 ./src/graph_compute/compute "$convergence_factor" \
+    $hilbert_inputs
+    >./results/${input_name}-hilbert-parallel-convergence-chroma.txt
+
+
+make clean
+make PARALLEL=1 build-libgraphio
+make D1_LOCKS=1 $default_compute_params build-graph-compute
+taskset -c 0-47 ./src/graph_compute/compute "$convergence_factor" \
+    $hilbert_inputs
+    >./results/${input_name}-hilbert-parallel-convergence-locks.txt
+
+
+make clean
+make PARALLEL=1 build-libgraphio
+make D1_PRIO=1 $default_compute_params build-graph-compute
+taskset -c 0-47 ./src/graph_compute/compute "$convergence_factor" \
+    $hilbert_inputs
+    >./results/${input_name}-hilbert-parallel-convergence-jp.txt
