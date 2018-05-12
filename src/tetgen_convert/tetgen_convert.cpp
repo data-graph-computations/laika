181897fbb562c1e68a2be219c6a39ab59e14a969
Predrag Gruevski <predrag@kensho.com>
2018-05-12 19:03:19 -0400

    Make the tetgen conversion script O(n) instead of O(n log n)

--- src/tetgen_convert/tetgen_convert.cpp
+++ src/tetgen_convert/tetgen_convert.cpp
@@ -3,18 +3,26 @@
 #include <string>
 #include <cstring>
 #include <stdexcept>
-#include <set>
+#include <unordered_set>
 
 using namespace std;
 
+
+struct pair_hash {
+  inline std::size_t operator()(const std::pair<int,int>& value) const {
+      return value.first ^ value.second;
+  }
+};
+
+
 void rewriteEdges(int numVertices, FILE * eleInputFile, FILE * eleOutputFile) {
-  set<pair<int, int> > edges;
+  unordered_set<pair<int, int>, pair_hash> edges;
   int numTet, g0, g1;
   fscanf(eleInputFile, "%d %d %d\n", &numTet, &g0, &g1);
   int tetId;
   int v[4];
   for (int i = 0; i < numTet; i++) {
-    fscanf(eleInputFile, "%d %d %d %d %d\n", &tetId, &v[0], &v[1], &v[2], &v[3]); 
+    fscanf(eleInputFile, "%d %d %d %d %d\n", &tetId, &v[0], &v[1], &v[2], &v[3]);
     for (int j = 0; j < 4; j++) {
       for (int k = j + 1; k < 4; k++) {
         edges.insert(pair<int, int>(v[j], v[k]));
@@ -24,9 +32,11 @@ void rewriteEdges(int numVertices, FILE * eleInputFile, FILE * eleOutputFile) {
   }
   printf("%d nodes, %d edges\n", numVertices, (int) edges.size());
   fprintf(eleOutputFile, "AdjacencyGraph\n%d\n%d\n", numVertices, (int) edges.size());
-  int lastSource = -1; 
+  int lastSource = -1;
   int count = 0;
-  for (set<pair<int, int> >::iterator it = edges.begin(); it != edges.end(); it++) {
+  for (unordered_set<pair<int, int>, pair_hash>::iterator it = edges.begin();
+       it != edges.end();
+       it++) {
     if (lastSource != it->first) {
       if (lastSource + 1 != it->first) {
         for (int i = lastSource + 1; i < it->first; i++) {
@@ -38,12 +48,14 @@ void rewriteEdges(int numVertices, FILE * eleInputFile, FILE * eleOutputFile) {
     lastSource = it->first;
     count++;
   }
-  
+
   for (int i = lastSource + 1; i < numVertices; i++) {
     fprintf(eleOutputFile, "%d\n", count);
   }
 
-  for (set<pair<int, int> >::iterator it = edges.begin(); it != edges.end(); it++) {
+  for (unordered_set<pair<int, int>, pair_hash>::iterator it = edges.begin();
+       it != edges.end();
+       it++) {
     fprintf(eleOutputFile, "%d\n", it->second);
   }
 }
